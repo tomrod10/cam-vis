f945c2f87c6ce4dba2ead43ab0eeba558e8952e6
Tomas Rodriguez <tomasrodriguez@Tomass-MacBook-Air.local>
2025-03-08 21:40:16 -0800

    add contour detection

--- app/display.py
+++ app/display.py
@@ -1,6 +1,7 @@
 import cv2
 import Processing
 
+
 class Display:
     def __init__(self):
         self.cap = cv2.VideoCapture(0)
@@ -9,8 +10,6 @@ class Display:
         if not self.cap.isOpened():
             print("Cannot open camera")
             exit()
-        
-
 
     def start(self):
         while True:
@@ -21,13 +20,13 @@ class Display:
                 break
 
             fgMask = self.md.background_subtraction(self.cap, frame)
-            cv2.imshow('frame', frame)
-            cv2.imshow('FG Mask', fgMask)
+            frame_ct = self.md.contour_detection(frame, fgMask)
+            cv2.imshow("FG Mask", frame_ct)
 
             if cv2.waitKey(1) == ord("q"):
                 self.stop()
                 break
-    
+
     def stop(self):
         self.cap.release()
         cv2.destroyAllWindows()
--- app/processing.py
+++ app/processing.py
@@ -1,21 +1,33 @@
 import cv2
 
 
-class MotionDetector():
+class MotionDetector:
     def __init__(self):
         self.backSub = cv2.createBackgroundSubtractorMOG2()
 
     def alert(self):
         print("Motion detected!")
 
-    def detect(self):
-        ...
-
     def background_subtraction(self, cap, frame):
         fgMask = self.backSub.apply(frame)
-        cv2.rectangle(frame, (10, 2), (100,20), (255,255,255), -1)
-        cv2.putText(frame, str(cap.get(cv2.CAP_PROP_POS_FRAMES)), (15, 15),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.5 , (0,0,0))
-        
+        cv2.rectangle(frame, (10, 2), (100, 20), (255, 255, 255), -1)
+        cv2.putText(
+            frame,
+            str(cap.get(cv2.CAP_PROP_POS_FRAMES)),
+            (15, 15),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            0.5,
+            (0, 0, 0),
+        )
         return fgMask
 
+    def contour_detection(self, frame, fg_mask):
+        cont, hier = cv2.findContours(
+            fg_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE
+        )
+        frame_ct = cv2.drawContours(frame, cont, -1, (0, 255, 0), 2)
+        return frame_ct
+
+    def threshold(self, frame): ...
+
+    def gaussian_blur(self): ...
